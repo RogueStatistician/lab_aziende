89c235bbf259499fef83ea429f6a90ec42d71330
RogueStatistician <simone.tosato95@gmail.com>
2019-12-17 13:30:28 +0100

    cose da creare

---
+++ cose_da_creare.R
@@ -0,0 +1,14 @@
+## Category unit sales (c, t)
+# vendite totali per una classe c nella settimana t 
+
+## Fattore di stagionalita' per Category unit sales
+# media delle vendite nella settimana t per quella classe
+# diviso le vendite medie per quella categoria tra tutte le settimane
+
+## Gross lift
+# differenza di vendite di quando un prodotto e' in promo e quando lo stesso
+# non e' in promo nella settimana t
+
+## Total gross lift c,t
+# somma dei gross lift tra tutti i prodotti appartenenti alla classe
+# c a prescindere dalla promo nella settimana t
